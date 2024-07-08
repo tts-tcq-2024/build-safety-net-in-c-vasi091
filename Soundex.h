8f139815b47eb4bc5dc4e4d64cfff13792c60b44
vasi091 <vasishtamukthinutalapati@gmail.com>
2024-07-08 15:00:40 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -5,16 +5,15 @@
 #include <string.h>
 
 char getSoundexCode(char c) {
+    static const char soundexTable[26] = {
+        '0', '1', '2', '3', '0', '1', '2', '0', '0', '2', '2', '4', '5', '5', '0', '1', '2', '6', '2', '3', '0', '1', '0', '2', '0', '2'
+    };
+
     c = toupper(c);
-    switch (c) {
-        case 'B': case 'F': case 'P': case 'V': return '1';
-        case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
-        case 'D': case 'T': return '3';
-        case 'L': return '4';
-        case 'M': case 'N': return '5';
-        case 'R': return '6';
-        default: return '0'; // For A, E, I, O, U, H, W, Y
+    if (c >= 'A' && c <= 'Z') {
+        return soundexTable[c - 'A'];
     }
+    return '0'; // For non-alphabet characters
 }
 
 void appendCode(char *soundex, int *sIndex, char code) {
